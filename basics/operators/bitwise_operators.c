c901a7a1b5f23649c2dbee3743d61326d8ea96c8
Anamaya Saraogi <112614436+AnamayaSaraogi@users.noreply.github.com>
2025-01-06 11:50:56 +0530

    Add files via upload

---
+++ basics/operators/bitwise_operators.c
@@ -0,0 +1,11 @@
+#include <stdio.h> 
+int main() 
+{ 
+    int a = 12, b = 25; 
+    printf("bitwise and = %d\n", a&b); 
+    printf("bitwise or = %d\n", a|b); 
+    printf("bitwise xor = %d\n", a^b); 
+    printf("complement = %d\n",~35); 
+    printf("complement = %d\n",~-12); 
+    return 0; 
+} 
